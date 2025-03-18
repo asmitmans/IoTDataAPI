9a2e7d3fe123f0b49d93e48019bb27757e0f0be9
asmitmans <a.smitmans@gmail.com>
2025-03-18 18:09:02 -0300

    Corrigiendo configuración de .gitattributes

---
+++ src/main/java/com/futuro/iotdataapi/IotDataApiApplication.java
@@ -0,0 +1,13 @@
+package com.futuro.iotdataapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotDataApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotDataApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/futuro/iotdataapi/IotDataApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futuro.iotdataapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotDataApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
